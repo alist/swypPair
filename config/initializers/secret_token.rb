e0f03f0c6dceefcaa8489400a9d8d7b51b51e422
Alexander List <alist@listprojects.com>
2011-12-22 15:09:19 -0500

    added example rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwypPair::Application.config.secret_token = '5059ba9c5be429d1a1f8f6ef173a9d64fcc51c0cb9d21a4f209a6d8e5c3de5991424097b0bca30597008b43d97bb9e5c8d956412298c487d6aefe1776916c04d'
