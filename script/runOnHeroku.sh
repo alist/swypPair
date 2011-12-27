c853d4fa3caf3a778ce137fd6d76aedf887374cf
Alexander List <alist@listprojects.com>
2011-12-27 10:50:00 -0500

    autoPush to heroku

--- script/runOnHeroku.sh
+++ script/runOnHeroku.sh
@@ -3,7 +3,7 @@ dir=`pwd`
 if [ -r "runOnHeroku.sh" ]; then
 	cd ..
 fi
-bundle exec rake assets:precompile
+#bundle exec rake assets:precompile
 git add .
 git commit -m "autoPush to heroku"
 git push heroku master
