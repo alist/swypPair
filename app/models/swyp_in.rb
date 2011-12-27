f87220909ba6e707a19cb170058c95e08a5b5fad
Alexander List <alist@listprojects.com>
2011-12-27 15:33:07 -0500

    autoPush to heroku

--- app/models/swyp_in.rb
+++ app/models/swyp_in.rb
@@ -2,7 +2,7 @@ class SwypIn < ActiveRecord::Base
 	has_one :swypOut
 	
 	def pair
-		SwypIn.swypOut = swypOut.find(3)
+		SwypIn.swypOut = SwypOut.find(3)
 		
 		if SwypIn.swypOut
 			puts "paired swypIn w/ id", SwypIn.id, "w/ swypOut id'd: ", SwypIn.swypOut.id
